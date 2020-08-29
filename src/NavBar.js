efb2334cbead9ea37122263649df3bb7fd6e9ab2
nadeemkhanrtm1 <nadeemkhanrtm@gmail.com>
2020-08-29 11:45:44 +0530

    deploy

---
+++ src/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Logo from "./images/logo.png";
+import david from "./images/david.jpg";
+
+const NavBar = () => {
+    return(
+        <>
+        <header>
+        <nav className="navbar">
+          <div className="logo_items">
+              <img src={Logo}></img>
+              <a>HOME</a>
+              <a>PROFILE</a>
+              <a>SCHEDULE APPOINTMENTS</a>
+          </div>
+          <div  className="profile_nav">
+              <p>Hello!<br/>
+              Dmitry Galkin</p>
+              <img src={david}></img>
+          </div>
+        </nav>
+      </header>
+        </>
+    ) 
+}
+
+export default NavBar;
